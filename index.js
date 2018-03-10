387ddc549dd098c0917cd3dddd4d59eae0e9fc84
Leonardo Dino <leonardo@kunstdesign.com.br>
2018-03-10 03:07:30 -0300

    cleanup

--- index.js
+++ index.js
@@ -2,7 +2,8 @@ const Packer = require('./lib/Packer.js')
 
 module.exports = (options = {}) => (sprites = []) => {
 	const instance = new Packer(options)
-	if(!instance.validate_params()) return null
-	if(!instance.load_sprites_info(sprites)) return null
-	return instance.compute_results()
+
+	return instance
+		.load_sprites_info(sprites)
+		.compute_results()
 }
